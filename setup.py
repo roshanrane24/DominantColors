e5fa65e6bf71fdaa7dc01ea9aa9f97db32b0e16c
Roshan Rane <roshan.rane97@gmail.com>
2021-06-02 00:11:40 +0530

    build

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="DominantColors",
+    version="0.1",
+    author="Roshan Rane",
+    author_email="roshan.ran3@gmail.com",
+    description="A package to extract dominant colors from an image.",
+    long_description=long_description,
+    url="https://github.com/roshanrane24/DominantColors",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independant"
+    ],
+    package_dir={"": "."},
+    packages=find_packages(where="."),
+    python_requires=">=3.6"
+)
