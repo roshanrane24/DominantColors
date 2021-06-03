55ee6c6a275410508364d36e7bd32faaa960c3cb
Roshan Rane <roshan.rane97@gmail.com>
2021-06-03 23:00:54 +0530

    Function to resize(Less work for algorithm)

--- dominant_colors/utils/image.py
+++ dominant_colors/utils/image.py
@@ -1,11 +1,13 @@
 from typing import Union
 
 import cv2
-import numpy as np
+from numpy import ndarray, moveaxis
 
 
 class Image(object):
-    def __init__(self, image_path: str, channel_first: bool = False):
+    def __init__(self,
+                 image_path: str,
+                 channel_first: bool = False):
         self.channel_first = channel_first
         self.image = self.read(image_path)
         if self.image is not None:
@@ -14,25 +16,25 @@ class Image(object):
             self.height, self.width = 0, 0
 
     @staticmethod
-    def read(image_path: str) -> Union[np.ndarray, None]:
+    def read(image_path: str) -> Union[ndarray, None]:
         return cv2.imread(image_path)
 
-    def get_rgb(self) -> np.ndarray:
+    def get_rgb(self) -> ndarray:
         if self.channel_first:
-            return np.moveaxis(cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB),
-                               2, 0)
+            return moveaxis(cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB),
+                            2, 0)
         else:
             return cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
 
-    def get_hsv(self) -> np.ndarray:
+    def get_hsv(self) -> ndarray:
         if self.channel_first:
-            return np.moveaxis(cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV),
-                               2, 0)
+            return moveaxis(cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV),
+                            2, 0)
         else:
             return cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
 
     @staticmethod
-    def show_image(image: np.ndarray, w_name: str = 'image') -> None:
+    def show_image(image: ndarray, w_name: str = 'image') -> None:
         while True:
             cv2.imshow(w_name, image)
             key = cv2.waitKey(1)
@@ -43,3 +45,9 @@ class Image(object):
     def show(self) -> None:
         if self.image is not None:
             self.show_image(self.image)
+
+    def resize(self, width: int = 1280, height: int = 720):
+        self.image = cv2.resize(self.image,
+                                (width, height),
+                                interpolation=cv2.INTER_AREA)
+        self.height, self.width = self.image.shape[:-1]
