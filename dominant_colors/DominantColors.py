5d6636041ac7504e5489c1def048a41e1fe4569d
Roshan Rane <roshan.rane97@gmail.com>
2021-06-03 23:10:05 +0530

    fixes for image resize & utils changes

--- dominant_colors/DominantColors.py
+++ dominant_colors/DominantColors.py
@@ -4,20 +4,19 @@ from sklearn.cluster import KMeans
 
 
 class DominantColors:
-    def __init__(self, path: str, no_of_colors: int = 3):
+    def __init__(self, path: str, no_of_colors: int = 5):
         self.image: Image = Image(path)
         self.clusters = no_of_colors
         self.colors: ndarray = self.get_colors()
 
-    @staticmethod
-    def colors_kmeans(image: Image, clusters: int = 3) -> KMeans:
-        kmean = KMeans(n_clusters=clusters, random_state=42)
-        kmean.fit(image.get_rgb().reshape(image.width * image.height, 3))
+    def colors_kmeans(self) -> KMeans:
+        kmean = KMeans(n_clusters=self.clusters, random_state=42)
+        self.image.resize()
+        kmean.fit(self.image.get_rgb().reshape(self.image.width * self.image.height, 3))
         return kmean
 
     def get_colors(self) -> ndarray:
         if self.image.image is not None:
-            return self.colors_kmeans(self.image,
-                                      clusters=self.clusters).cluster_centers_.astype(uint8)
+            return self.colors_kmeans().cluster_centers_.astype(uint8)
         else:
             return ones((3, 3), dtype=uint8) * 255
